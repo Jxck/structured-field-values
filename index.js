57163c1789b5ca4501eaabb65d8194c038e28270
Jxck <block.rxckin.beats@gmail.com>
2020-09-26 03:47:11 +0900

    first impl

---
+++ index.js
@@ -0,0 +1,295 @@
+"use strict";
+import assert from "assert";
+let tmp
+const ok = true
+
+const log = console.log.bind(console)
+
+function token(reg) {
+  return (rest) => {
+    const result = reg.exec(rest)
+    if (result === null) {
+      return {ok: false, rest}
+    } else {
+      const value = result[0]
+      return {ok, value, rest: rest.substr(value.length)}
+    }
+  }
+}
+
+function test_token() {
+  const fn = token(/^abc/)
+  assert.deepEqual(
+    fn("abc"),
+    {ok, value: "abc", rest: ""}
+  )
+  assert.deepEqual(
+    fn("abd"),
+    {ok: false, rest: "abd"}
+  )
+}
+
+function alt(fns) {
+  return (rest) => {
+    for (let i = 0; i < fns.length; i ++) {
+      const result = fns[i](rest)
+      if (result.ok) {
+        return result
+      }
+    }
+    return {ok: false, rest}
+  }
+}
+
+function test_alt() {
+  // a / b
+  const fn = alt([token(/^a/), token(/^b/)])
+  assert.deepEqual(
+    fn("a"),
+    {ok, value: "a", rest: ""}
+  )
+  assert.deepEqual(
+    fn("b"),
+    {ok, value: "b", rest: ""}
+  )
+  assert.deepEqual(
+    fn("c"),
+    {ok: false, rest: "c"}
+  )
+}
+
+function list(fns) {
+  return (rest) => {
+    const value = []
+    const orig  = rest
+    for (let i = 0; i < fns.length; i ++) {
+      const result = fns[i](rest)
+      if (result.ok) {
+        value.push(result.value)
+        rest = result.rest
+        if (rest.length === 0) break
+      }
+    }
+
+    if (value.length == fns.length) {
+      return {ok, value, rest}
+    } else {
+      return {ok: false, rest: orig}
+    }
+  }
+}
+
+function test_list() {
+  // [a, b, c]
+  let fn = list([token(/^a/), token(/^b/), token(/^c/)])
+  assert.deepEqual(
+    fn("abc"),
+    {ok, value: ["a", "b", "c"], rest: ""}
+  )
+  assert.deepEqual(
+    fn("axc"),
+    {ok: false, rest: "axc"}
+  )
+
+  // [ab, cde, f]
+  fn = list([token(/^ab/), token(/^cde/), token(/^f/)])
+  assert.deepEqual(
+    fn("abcdef"),
+    {ok, value: ["ab", "cde", "f"], rest: ""}
+  )
+  assert.deepEqual(
+    fn("abcde"),
+    {ok: false, rest: "abcde"}
+  )
+}
+
+function repeat(min, max, fn) {
+  return (rest) => {
+    const value = []
+    const found = 0
+    const orig  = rest
+    while(true) {
+      const result = fn(rest)
+      if (result.ok) {
+        value.push(result.value)
+        rest = result.rest
+        if (value.length === max) break
+      } else {
+        break
+      }
+    }
+
+    if (value.length <= min) {
+      return {ok: false, rest: orig}
+    } else {
+      return {ok, value: value.join(""), rest}
+    }
+  }
+}
+
+function test_repeat() {
+  let fn = repeat(0, 5, alt([token(/^a/), token(/^b/)]))
+  assert.deepEqual(
+    fn("a"),
+    {ok, value: "a", rest: ""}
+  )
+  assert.deepEqual(
+    fn("ab"),
+    {ok, value: "ab", rest: ""}
+  )
+  assert.deepEqual(
+    fn("b"),
+    {ok, value: "b", rest: ""}
+  )
+  assert.deepEqual(
+    fn("c"),
+    {ok:false, rest: "c"}
+  )
+  assert.deepEqual(
+    fn("aabaab"),
+    {ok, value: "aabaa", rest: "b"}
+  )
+  assert.deepEqual(
+    fn("aacaaab"),
+    {ok, value: "aa", rest: "caaab"}
+  )
+}
+
+function sf_integer() {
+  return (rest) => {
+    const result = token(/^\-{0,1}\d{1,15}/)(rest)
+    if (result.ok) {
+      return {ok, value: parseInt(result.value), rest: result.rest}
+    } else {
+      return {ok: false, rest}
+    }
+  }
+}
+
+function test_sf_integer() {
+  assert.deepEqual(
+    sf_integer()("42"),
+    {ok, value: 42, rest: ""}
+  )
+  assert.deepEqual(
+    sf_integer()("-42"),
+    {ok, value: -42, rest: ""}
+  )
+  assert.deepEqual(
+    sf_integer()("4.2"),
+    {ok, value: 4, rest: ".2"}
+  )
+  assert.deepEqual(
+    sf_integer()("4a"),
+    {ok, value: 4, rest: "a"}
+  )
+  assert.deepEqual(
+    sf_integer()("a"),
+    {ok: false, rest: "a"}
+  )
+}
+
+function sf_decimal() {
+  return (rest) => {
+    const result = token(/^\-{0,1}\d{1,12}\.\d{1,3}/)(rest)
+    if (result.ok) {
+      return {ok, value: parseFloat(result.value), rest: result.rest}
+    } else {
+      return {ok: false, rest}
+    }
+  }
+}
+
+function test_sf_decimal() {
+  assert.deepEqual(
+    sf_decimal()("4.5"),
+    {ok, value: 4.5, rest: ""}
+  )
+  assert.deepEqual(
+    sf_decimal()("-4.5"),
+    {ok, value: -4.5, rest: ""}
+  )
+  assert.deepEqual(
+    sf_decimal()("4.0"),
+    {ok, value: 4, rest: ""}
+  )
+  assert.deepEqual(
+    sf_decimal()("45"),
+    {ok: false, rest: "45"}
+  )
+}
+
+function sf_string() {
+  return function(rest) {
+    const fn = list([
+      token(/^"/),
+      repeat(0, 1024, sf_char()),
+      token(/^"/),
+    ])
+    const result = fn(rest)
+    if (result.ok) {
+      result.value = result.value.join('')
+      return result
+    } else {
+      return result
+    }
+  }
+}
+
+function test_sf_string() {
+  assert.deepEqual(sf_string()(`"asdf"`),  {ok, value: `"asdf"`,  rest: ``})
+  assert.deepEqual(sf_string()(`"a""`),    {ok, value: `"a"`,     rest: `"`})
+  assert.deepEqual(sf_string()(`"a\\\""`), {ok, value: `"a\\\""`, rest: ``})
+  assert.deepEqual(sf_string()(`"a\\"`),   {ok: false, rest: `"a\\"`})
+  assert.deepEqual(sf_string()(`"a\\\\c"`),{ok, value: `"a\\\\c"`, rest: ``})
+}
+
+function sf_char() {
+  return alt([
+    escaped(),
+    unescaped(),
+  ])
+}
+
+function test_sf_char() {
+  assert.deepEqual(sf_char()('"'),    {ok: false,        rest: '"'})
+  assert.deepEqual(sf_char()("\\\""), {ok, value: '\\\"', rest: ''})
+  assert.deepEqual(sf_char()("\\\\"), {ok, value: '\\\\', rest: ''})
+}
+
+function unescaped() {
+  return token(/^([\x20-\x21])|([\x23-\x5B])|([\x5D-\x7E])/)
+}
+
+function test_unescaped() {
+  assert.deepEqual(unescaped()(" "),  {ok, value: ' ', rest: ''})   // x20
+  assert.deepEqual(unescaped()("!"),  {ok, value: '!', rest: ''})   // x21
+  assert.deepEqual(unescaped()('"'),  {ok: false,      rest: '"'})  // x22
+  assert.deepEqual(unescaped()("#"),  {ok, value: '#', rest: ''})   // x23
+  assert.deepEqual(unescaped()("["),  {ok, value: '[', rest: ''})   // x5B
+  assert.deepEqual(unescaped()(`\\`), {ok: false,      rest: '\\'}) // x5C
+  assert.deepEqual(unescaped()("]"),  {ok, value: ']', rest: ''})   // x5D
+}
+
+function escaped() {
+  return token(/^((\\\")|(\\\\))/)
+}
+
+function test_escaped() {
+  assert.deepEqual(escaped()(`\\\"`), {ok, value: `\\\"`, rest: ''})
+  assert.deepEqual(escaped()(`\\\\`), {ok, value: `\\\\`, rest: ''})
+}
+
+
+test_token()
+test_alt()
+test_list()
+test_repeat()
+
+test_sf_integer()
+test_sf_decimal()
+
+test_sf_string()
+test_sf_char()
+test_unescaped()
+test_escaped()
