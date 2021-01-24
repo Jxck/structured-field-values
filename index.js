5a2090c359f1686d0c2b9d3c672d1a0e54f90092
jxck <block.rxckin.beats@gmail.com>
2021-01-25 03:36:02 +0900

    add serializer comments

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const ok = true
 /////////////////////////
 // public interface
 /////////////////////////
+// 4.1.  Serializing Structured Fields
+//
+// 1.  If the structure is a Dictionary or List and its value is empty
+//     (i.e., it has no members), do not serialize the field at all
+//     (i.e., omit both the field-name and field-value).
+//
+// 2.  If the structure is a List, let output_string be the result of
+//     running Serializing a List (Section 4.1.1) with the structure.
+//
+// 3.  Else if the structure is a Dictionary, let output_string be the
+//     result of running Serializing a Dictionary (Section 4.1.2) with
+//     the structure.
+//
+// 4.  Else if the structure is an Item, let output_string be the result
+//     of running Serializing an Item (Section 4.1.3) with the
+//     structure.
+//
+// 5.  Else, fail serialization.
+//
+// 6.  Return output_string converted into an array of bytes, using
+//     ASCII encoding [RFC0020].
+
+
 export function decodeItem(value) { return parseItem(value) }
 export function decodeList(value) { return parseList(value) }
 export function decodeDict(value) { return parseDict(value) }
@@ -11,7 +34,157 @@ export function encodeItem(value) { return serializeItem(value) }
 export function encodeList(value) { return serializeList(value) }
 export function encodeDict(value) { return serializeDict(value) }
 
-/// serializer
+
+// 4.1.1.  Serializing a List
+//
+// Given an array of (member_value, parameters) tuples as input_list,
+// return an ASCII string suitable for use in a HTTP field value.
+//
+// 1.  Let output be an empty string.
+//
+// 2.  For each (member_value, parameters) of input_list:
+//
+//     1.  If member_value is an array, append the result of running
+//         Serializing an Inner List (Section 4.1.1.1) with
+//         (member_value, parameters) to output.
+//
+//     2.  Otherwise, append the result of running Serializing an Item
+//         (Section 4.1.3) with (member_value, parameters) to output.
+//
+//     3.  If more member_values remain in input_list:
+//
+//         1.  Append "," to output.
+//
+//         2.  Append a single SP to output.
+//
+// 3.  Return output.
+export function serializeList(list) {
+  return list.map(({value, params}) => {
+    if (Array.isArray(value)) {
+      return serializeInnerList({value, params})
+    }
+    return serializeItem({value, params})
+  }).join(", ")
+}
+
+// 4.1.1.1.  Serializing an Inner List
+//
+// Given an array of (member_value, parameters) tuples as inner_list,
+// and parameters as list_parameters, return an ASCII string suitable
+// for use in a HTTP field value.
+//
+// 1.  Let output be the string "(".
+//
+// 2.  For each (member_value, parameters) of inner_list:
+//
+//     1.  Append the result of running Serializing an Item
+//         (Section 4.1.3) with (member_value, parameters) to output.
+//
+//     2.  If more values remain in inner_list, append a single SP to
+//         output.
+//
+// 3.  Append ")" to output.
+//
+// 4.  Append the result of running Serializing Parameters
+//     (Section 4.1.1.2) with list_parameters to output.
+//
+// 5.  Return output.
+export function serializeInnerList({value, params}) {
+  return `(${value.map(serializeItem).join(" ")})${serializeParams(params)}`
+}
+
+// 4.1.1.2.  Serializing Parameters
+//
+// Given an ordered Dictionary as input_parameters (each member having a
+// param_name and a param_value), return an ASCII string suitable for
+// use in a HTTP field value.
+//
+// 1.  Let output be an empty string.
+//
+// 2.  For each param_name with a value of param_value in
+//     input_parameters:
+//
+//     1.  Append ";" to output.
+//
+//     2.  Append the result of running Serializing a Key
+//         (Section 4.1.1.3) with param_name to output.
+//
+//     3.  If param_value is not Boolean true:
+//
+//         1.  Append "=" to output.
+//
+//         2.  Append the result of running Serializing a bare Item
+//             (Section 4.1.3.1) with param_value to output.
+//
+// 3.  Return output.
+export function serializeParams(params) {
+  return Object.entries(params).map(([key, value]) => {
+    if (value === true) return `;${key}` // omit true
+    return `;${serializeKey(key)}=${serializeBareItem(value)}`
+  }).join("")
+}
+
+// 4.1.1.3.  Serializing a Key
+//
+// Given a key as input_key, return an ASCII string suitable for use in
+// a HTTP field value.
+//
+// 1.  Convert input_key into a sequence of ASCII characters; if
+//     conversion fails, fail serialization.
+//
+// 2.  If input_key contains characters not in lcalpha, DIGIT, "_", "-",
+//     ".", or "*" fail serialization.
+//
+// 3.  If the first character of input_key is not lcalpha or "*", fail
+//     serialization.
+//
+// 4.  Let output be an empty string.
+//
+// 5.  Append input_key to output.
+//
+// 6.  Return output.
+export function serializeKey(key) {
+  return key
+}
+
+// 4.1.2.  Serializing a Dictionary
+//
+// Given an ordered Dictionary as input_dictionary (each member having a
+// member_name and a tuple value of (member_value, parameters)), return
+// an ASCII string suitable for use in a HTTP field value.
+//
+// 1.  Let output be an empty string.
+//
+// 2.  For each member_name with a value of (member_value, parameters)
+//     in input_dictionary:
+//
+//     1.  Append the result of running Serializing a Key
+//         (Section 4.1.1.3) with member's member_name to output.
+//
+//     2.  If member_value is Boolean true:
+//
+//         1.  Append the result of running Serializing Parameters
+//             (Section 4.1.1.2) with parameters to output.
+//
+//     3.  Otherwise:
+//
+//         1.  Append "=" to output.
+//
+//         2.  If member_value is an array, append the result of running
+//             Serializing an Inner List (Section 4.1.1.1) with
+//             (member_value, parameters) to output.
+//
+//         3.  Otherwise, append the result of running Serializing an
+//             Item (Section 4.1.3) with (member_value, parameters) to
+//             output.
+//
+//     4.  If more members remain in input_dictionary:
+//
+//         1.  Append "," to output.
+//
+//         2.  Append a single SP to output.
+//
+// 3.  Return output.
 export function serializeDict(dict) {
   return Object.entries(dict).map(([key, {value, params}]) => {
     let output = serializeKey(key)
@@ -29,66 +202,201 @@ export function serializeDict(dict) {
   }).join(", ")
 }
 
-
+// 4.1.3.  Serializing an Item
+//
+// Given an Item as bare_item and Parameters as item_parameters, return
+// an ASCII string suitable for use in a HTTP field value.
+//
+// 1.  Let output be an empty string.
+//
+// 2.  Append the result of running Serializing a Bare Item
+//     Section 4.1.3.1 with bare_item to output.
+//
+// 3.  Append the result of running Serializing Parameters
+//     Section 4.1.1.2 with item_parameters to output.
+//
+// 4.  Return output.
 export function serializeItem({value, params}) {
   return `${serializeBareItem(value)}${serializeParams(params)}`
 }
 
-export function serializeList(list) {
-  return list.map(({value, params}) => {
-    if (Array.isArray(value)) {
-      return serializeInnerList({value, params})
-    }
-    return serializeItem({value, params})
-  }).join(", ")
-}
-
-export function serializeInnerList({value, params}) {
-  return `(${value.map(serializeItem).join(" ")})${serializeParams(params)}`
-}
-
-
+// 4.1.3.1.  Serializing a Bare Item
+//
+// Given an Item as input_item, return an ASCII string suitable for use
+// in a HTTP field value.
+//
+// 1.  If input_item is an Integer, return the result of running
+//     Serializing an Integer (Section 4.1.4) with input_item.
+//
+// 2.  If input_item is a Decimal, return the result of running
+//     Serializing a Decimal (Section 4.1.5) with input_item.
+//
+// 3.  If input_item is a String, return the result of running
+//     Serializing a String (Section 4.1.6) with input_item.
+//
+// 4.  If input_item is a Token, return the result of running
+//     Serializing a Token (Section 4.1.7) with input_item.
+//
+// 5.  If input_item is a Boolean, return the result of running
+//     Serializing a Boolean (Section 4.1.9) with input_item.
+//
+// 6.  If input_item is a Byte Sequence, return the result of running
+//     Serializing a Byte Sequence (Section 4.1.8) with input_item.
+//
+// 7.  Otherwise, fail serialization.
 export function serializeBareItem(value) {
   switch (typeof value) {
     case "number":
-      // Serializing an Integer/Decimal
-      // 1.  If input_integer is not an integer in the range of -999,999,999,999,999 to 999,999,999,999,999 inclusive, fail serialization.
+      // 4.1.4.  Serializing an Integer
+      //
+      // Given an Integer as input_integer, return an ASCII string suitable
+      // for use in a HTTP field value.
+      //
+      // 1.  If input_integer is not an integer in the range of
+      //     -999,999,999,999,999 to 999,999,999,999,999 inclusive, fail
+      //     serialization.
+      //
+      // 2.  Let output be an empty string.
+      //
+      // 3.  If input_integer is less than (but not equal to) 0, append "-" to
+      //     output.
+      //
+      // 4.  Append input_integer's numeric value represented in base 10 using
+      //     only decimal digits to output.
+      //
+      // 5.  Return output.
+      //
+      // 4.1.5.  Serializing a Decimal
+      //
+      // Given a decimal number as input_decimal, return an ASCII string
+      // suitable for use in a HTTP field value.
+      //
+      // 1.   If input_decimal is not a decimal number, fail serialization.
+      //
+      // 2.   If input_decimal has more than three significant digits to the
+      //      right of the decimal point, round it to three decimal places,
+      //      rounding the final digit to the nearest value, or to the even
+      //      value if it is equidistant.
+      //
+      // 3.   If input_decimal has more than 12 significant digits to the left
+      //      of the decimal point after rounding, fail serialization.
+      //
+      // 4.   Let output be an empty string.
+      //
+      // 5.   If input_decimal is less than (but not equal to) 0, append "-"
+      //      to output.
+      //
+      // 6.   Append input_decimal's integer component represented in base 10
+      //      (using only decimal digits) to output; if it is zero, append
+      //      "0".
+      //
+      // 7.   Append "." to output.
+      //
+      // 8.   If input_decimal's fractional component is zero, append "0" to
+      //      output.
+      //
+      // 9.   Otherwise, append the significant digits of input_decimal's
+      //      fractional component represented in base 10 (using only decimal
+      //      digits) to output.
+      //
+      // 10.  Return output.
       if (value < -999999999999999n || 999999999999999n < value) throw new Error(`fail serialization: ${value}`)
       return value.toString()
     case "string":
-      // Serializing an String
+      // 4.1.6.  Serializing a String
+      //
+      // Given a String as input_string, return an ASCII string suitable for
+      // use in a HTTP field value.
+      //
+      // 1.  Convert input_string into a sequence of ASCII characters; if
+      //     conversion fails, fail serialization.
+      //
+      // 2.  If input_string contains characters in the range %x00-1f or %x7f
+      //     (i.e., not in VCHAR or SP), fail serialization.
+      //
+      // 3.  Let output be the string DQUOTE.
+      //
+      // 4.  For each character char in input_string:
+      //
+      //     1.  If char is "\" or DQUOTE:
+      //
+      //         1.  Append "\" to output.
+      //
+      //     2.  Append char to output.
+      //
+      // 5.  Append DQUOTE to output.
+      //
+      // 6.  Return output.
       if (/[\x00-\x1f\x7f]+/.test(value)) throw new Error(`fail serialization: ${value}`)
       return `"${value.replace(/\\/g, `\\\\`).replace(/"/g, `\\\"`)}"`
-    case "boolean":
-      // Serializing an Boolean
-      return value ? "?1" : "?0"
     case "symbol":
-      // Serializing an Token
+      // 4.1.7.  Serializing a Token
+      //
+      // Given a Token as input_token, return an ASCII string suitable for use
+      // in a HTTP field value.
+      //
+      // 1.  Convert input_token into a sequence of ASCII characters; if
+      //     conversion fails, fail serialization.
+      //
+      // 2.  If the first character of input_token is not ALPHA or "*", or the
+      //     remaining portion contains a character not in tchar, ":" or "/",
+      //     fail serialization.
+      //
+      // 3.  Let output be an empty string.
+      //
+      // 4.  Append input_token to output.
+      //
+      // 5.  Return output.
       return Symbol.keyFor(value)
+    case "boolean":
+      // 4.1.9.  Serializing a Boolean
+      //
+      // Given a Boolean as input_boolean, return an ASCII string suitable for
+      // use in a HTTP field value.
+      //
+      // 1.  If input_boolean is not a boolean, fail serialization.
+      //
+      // 2.  Let output be an empty string.
+      //
+      // 3.  Append "?" to output.
+      //
+      // 4.  If input_boolean is true, append "1" to output.
+      //
+      // 5.  If input_boolean is false, append "0" to output.
+      //
+      // 6.  Return output.
+      return value ? "?1" : "?0"
     case "object":
       if (value instanceof Uint8Array) {
+        // 4.1.8.  Serializing a Byte Sequence
+        //
+        // Given a Byte Sequence as input_bytes, return an ASCII string suitable
+        // for use in a HTTP field value.
+        //
+        // 1.  If input_bytes is not a sequence of bytes, fail serialization.
+        //
+        // 2.  Let output be an empty string.
+        //
+        // 3.  Append ":" to output.
+        //
+        // 4.  Append the result of base64-encoding input_bytes as per
+        //     [RFC4648], Section 4, taking account of the requirements below.
+        //
+        // 5.  Append ":" to output.
+        //
+        // 6.  Return output.
+        //
+        // The encoded data is required to be padded with "=", as per [RFC4648],
+        // Section 3.2.
+        //
+        // Likewise, encoded data SHOULD have pad bits set to zero, as per
+        // [RFC4648], Section 3.5, unless it is not possible to do so due to
+        // implementation constraints.
         return `:${base64encode(value)}:`
       }
     default:
       break;
   }
-}
-
-export function serializeKey(key) {
-  // 1.  Convert input_key into a sequence of ASCII characters; if
-  //     conversion fails, fail serialization.
-  // 2.  If input_key contains characters not in lcalpha, DIGIT, "_", "-",
-  //     ".", or "*" fail serialization.
-  // 3.  If the first character of input_key is not lcalpha or "*", fail
-  //     serialization.
-  return key
-}
-
-export function serializeParams(params) {
-  return Object.entries(params).map(([key, value]) => {
-    if (value === true) return `;${key}` // omit true
-    return `;${serializeKey(key)}=${serializeBareItem(value)}`
-  }).join("")
 }
 
 
